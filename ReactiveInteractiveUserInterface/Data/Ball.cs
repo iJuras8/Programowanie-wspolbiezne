8d6d08f99e826104cbb435872cba53140140ca91
Pawel <paweljurczyk2004@gmail.com>
2026-04-21 20:40:15 +0200

    Ograniczenie ruchu kul

--- ReactiveInteractiveUserInterface/Data/Ball.cs
+++ ReactiveInteractiveUserInterface/Data/Ball.cs
@@ -1,50 +1,67 @@
-﻿//____________________________________________________________________________________________________________________________________
-//
-//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
-//
-//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
-//
-//  https://github.com/mpostol/TP/discussions/182
-//
-//_____________________________________________________________________________________________________________________________________
+﻿using System;
 
 namespace TP.ConcurrentProgramming.Data
 {
-  internal class Ball : IBall
-  {
-    #region ctor
-
-    internal Ball(Vector initialPosition, Vector initialVelocity)
+    internal class Ball : IBall
     {
-      Position = initialPosition;
-      Velocity = initialVelocity;
+        private readonly double _boardWidth = 400.0;
+        private readonly double _boardHeight = 420.0;
+        private readonly double _diameter = 20.0;
+
+        #region ctor
+
+        internal Ball(Vector initialPosition, Vector initialVelocity)
+        {
+            Position = initialPosition;
+            Velocity = initialVelocity;
+        }
+
+        #endregion ctor
+
+        #region IBall
+
+        public event EventHandler<IVector>? NewPositionNotification;
+
+        public IVector Velocity { get; set; }
+
+        #endregion IBall
+
+        #region private
+
+        private Vector Position;
+
+        private void RaiseNewPositionChangeNotification()
+        {
+            NewPositionNotification?.Invoke(this, Position);
+        }
+
+        internal void Move(Vector delta)
+        {
+            double newX = Position.x + delta.x;
+            double newY = Position.y + delta.y;
+
+            if (newX < 0)
+            {
+                newX = 0; 
+            }
+            else if (newX > _boardWidth - _diameter)
+            {
+                newX = _boardWidth - _diameter; 
+            }
+
+            if (newY < 0)
+            {
+                newY = 0; 
+            }
+            else if (newY > _boardHeight - _diameter)
+            {
+                newY = _boardHeight - _diameter; 
+            }
+
+            Position = new Vector(newX, newY);
+            RaiseNewPositionChangeNotification();
+        }
+
+        #endregion private
     }
-
-    #endregion ctor
-
-    #region IBall
-
-    public event EventHandler<IVector>? NewPositionNotification;
-
-    public IVector Velocity { get; set; }
-
-    #endregion IBall
-
-    #region private
-
-    private Vector Position;
-
-    private void RaiseNewPositionChangeNotification()
-    {
-      NewPositionNotification?.Invoke(this, Position);
-    }
-
-    internal void Move(Vector delta)
-    {
-      Position = new Vector(Position.x + delta.x, Position.y + delta.y);
-      RaiseNewPositionChangeNotification();
-    }
-
-    #endregion private
-  }
 }
